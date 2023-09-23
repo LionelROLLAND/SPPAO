84b0c197375c9231b7c75d78bb0b20aceac1bd17
LionelROLLAND <lionel.rolland@protonmail.com>
2023-09-23 18:22:38 +0200

    [edit] Remove unused cli arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -92,36 +92,37 @@ void writeAllComparison()
 	filesystem::path filepath;
 
 	// list<filesystem::path> ss_cl = list<filesystem::path>();
-	list<filesystem::path> ss_ads = list<filesystem::path>();
-	list<filesystem::path> ss_ads_rm = list<filesystem::path>();
+	list<filesystem::path> ss_st = list<filesystem::path>();
+	list<filesystem::path> ss_del = list<filesystem::path>();
 	list<filesystem::path> ss_add = list<filesystem::path>();
-	list<filesystem::path> ss_add_opt = list<filesystem::path>();
+	list<filesystem::path> ss_del_star = list<filesystem::path>();
+	list<filesystem::path> ss_add_star = list<filesystem::path>();
 
 	// filepath = filesystem::current_path()/"data";
 	// filepath = filesystem::current_path()/"data"/"results VM"/"res1";
-	filepath = filesystem::current_path() / "data" / "Saved results 3";
+	filepath = filesystem::current_path() / "data" / "last_results";
 
 	// ss_cl.push_back(filepath/"SS-CL_newDB.txt");
 	// ss_cl.push_back(filepath/"SS-CL_completeDB.txt");
 
-	// ss_ads.push_back(filepath/"SS-ST_newDB.txt");
-	ss_ads.push_back(filepath / "SS-ST_completeDB.txt");
+	// ss_st.push_back(filepath/"SS-ST_newDB.txt");
+	ss_st.push_back(filepath / "SS-ST_completeDB.txt");
 
-	// ss_ads_rm.push_back(filepath/"SS-DEL_newDB.txt");
-	ss_ads_rm.push_back(filepath / "SS-DEL_completeDB.txt");
+	// ss_del.push_back(filepath/"SS-DEL_newDB.txt");
+	ss_del.push_back(filepath / "SS-DEL_completeDB.txt");
 
-	// ss_add.push_back(filepath/"SS-ADD1_newDB.txt");
-	ss_add.push_back(filepath / "SS-ADD1_completeDB.txt");
+	// ss_add.push_back(filepath/"SS-ADD_newDB.txt");
+	ss_add.push_back(filepath / "SS-ADD_completeDB.txt");
 
-	// ss_add_opt.push_back(filepath/"SS-ADD2_newDB.txt");
-	ss_add_opt.push_back(filepath / "SS-ADD2_completeDB.txt");
+	// ss_add_opt.push_back(filepath/"SS-ADD-STAR_newDB.txt");
+	ss_add_star.push_back(filepath / "SS-ADD-STAR_completeDB.txt");
 
 	list<list<filesystem::path>> testsListSS = list<list<filesystem::path>>();
 	// testsListSS.push_back(ss_cl);
-	testsListSS.push_back(ss_ads);
-	testsListSS.push_back(ss_ads_rm);
+	testsListSS.push_back(ss_st);
+	testsListSS.push_back(ss_del);
 	testsListSS.push_back(ss_add);
-	testsListSS.push_back(ss_add_opt);
+	testsListSS.push_back(ss_add_star);
 
 	list<methodSS> *methodListSS = filesToResultSS(testsListSS);
 
@@ -134,39 +135,40 @@ void writeAllComparison()
 	*/
 
 	// string ID = "newDB";
-	string ID = "completeDB_s";
+	// string ID = "completeDB_s";
 	// string ID = "newDBVM";
 	// string ID = "completeDBVM";
+	string ID = "_completeDB";
 	filesystem::path outfilepath = filesystem::current_path();
 	outfilepath /= "data";
 	outfilepath /= "comparisonBS" + ID + ".tex";
 
-	list<filesystem::path> bs_cl = list<filesystem::path>();
-	list<filesystem::path> bs_ads = list<filesystem::path>();
+	// list<filesystem::path> bs_cl = list<filesystem::path>();
+	list<filesystem::path> bs_st = list<filesystem::path>();
 	// list<filesystem::path> bs_cstar = list<filesystem::path>();
-	list<filesystem::path> bs_ads_cstar = list<filesystem::path>();
+	list<filesystem::path> bs_lb = list<filesystem::path>();
 	// list<filesystem::path> bs_evo = list<filesystem::path>();
 
 	// bs_cl.push_back(filepath/"BS-CL_newDB.txt");
-	bs_cl.push_back(filepath / "BS-CL_completeDB.txt");
+	// bs_cl.push_back(filepath / "BS-CL_completeDB.txt");
 
-	// bs_ads.push_back(filepath/"BS-ST_newDB.txt");
-	bs_ads.push_back(filepath / "BS-ST_completeDB.txt");
+	// bs_st.push_back(filepath/"BS-ST_newDB.txt");
+	bs_st.push_back(filepath / "BS-ST_completeDB.txt");
 
 	// bs_cstar.push_back(filepath/"BS-CSTAR_newDB.txt");
 	// bs_cstar.push_back(filepath/"BS-CSTAR_completeDB.txt");
 
-	// bs_ads_cstar.push_back(filepath/"BS-LB_-ADSnewDB.txt");
-	bs_ads_cstar.push_back(filepath / "BS-LB_-ADScompleteDB.txt");
+	// bs_lb.push_back(filepath/"BS-LB_newDB.txt");
+	bs_lb.push_back(filepath / "BS-LB_completeDB.txt");
 
 	// bs_evo.push_back(filepath/"BS-EVO_newDB.txt");
 	// bs_evo.push_back(filepath/"BS-EVO_completeDB.txt");
 
 	list<list<filesystem::path>> testsListBS = list<list<filesystem::path>>();
 	// testsListBS.push_back(bs_cl);
-	testsListBS.push_back(bs_ads);
+	testsListBS.push_back(bs_st);
 	// testsListBS.push_back(bs_cstar);
-	testsListBS.push_back(bs_ads_cstar);
+	testsListBS.push_back(bs_lb);
 	// testsListBS.push_back(bs_evo);
 
 	list<methodBS> *methodListBS = filesToResultBS(testsListBS);
@@ -178,9 +180,10 @@ void writeAllComparison()
 	writing.close();
 
 	// ID = "newDB";
-	ID = "completeDB_s";
+	// ID = "completeDB_s";
 	// ID = "newDBVM";
 	// ID = "completeDBVM";
+	ID = "_completeDB";
 	outfilepath = filesystem::current_path();
 	outfilepath /= "data";
 	outfilepath /= "comparisonTime" + ID + ".tex";
@@ -207,7 +210,9 @@ void writeAllComparison()
 int main(int argc, char *argv[])
 {
 	po::options_description desc("Allowed options");
-	desc.add_options()("help", "produce help message")("p_square", po::value<double>()->default_value(0.5), "proportion of hexagons to be turned into squares")("p_merge", po::value<double>()->default_value(0.5), "proportion of nodes to be merged")("P", po::value<int>()->default_value(10), "height of the initial grid")("Q", po::value<int>()->default_value(10), "width of the initial grid")("O", po::value<int>()->default_value(5), "number of obstacles")("seed", po::value<int>()->default_value(time(nullptr)), "seed of the random generator")("v", "verbosity mode + records the rectangles");
+	desc.add_options()(
+		"help", "produce help message")(
+		"v", "verbosity mode + records the rectangles");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -221,16 +226,8 @@ int main(int argc, char *argv[])
 
 	logs = vm.count("v") ? true : false;
 
-	double p_square = vm["p_square"].as<double>();
-	double p_merge = vm["p_merge"].as<double>();
-	int P = vm["P"].as<int>();
-	int Q = vm["Q"].as<int>();
-	int O = vm["O"].as<int>();
-
-	cout << p_square << p_merge << P << Q << O << logs << endl;
-
-	int seed = vm["seed"].as<int>();
-	seed = 0;
+	cout << logs << endl;
+	// seed = 0;
 	// int seed = time(nullptr);
 	// int seed = 1654611373; ./output/main --P 30 --Q 30 --O 2 --seed 1654611373 > ./data/logs.log && cat ./data/logs.log | grep "Deleting path"
 	// int seed = 1654680670; ./output/main --P 100 --Q 100 --O 2 --p_merge 0 --p_square 1 --seed 1654680670 --v
@@ -238,9 +235,8 @@ int main(int argc, char *argv[])
 	// 1655724989;
 	// 1655748706;
 
-	srand(seed); // 1652869031
-	cout << "seed : " << seed << "\n\n"
-		 << endl;
+	// srand(seed); // 1652869031
+	// cout << "seed : " << seed << "\n\n" << endl;
 	// testSPPAO2(P, Q, O, p_square, p_merge);
 	// testDB();
 	// realDB();
