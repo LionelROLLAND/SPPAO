2c1e6a2f20764b7b14bbe384ec505ce88f0fb402
LionelROLLAND <lionel.rolland@protonmail.com>
2024-03-10 02:15:52 +0100

    [+] First version of the update operator.

--- src/single_search.cpp
+++ src/single_search.cpp
@@ -10,54 +10,14 @@ void update(Node *n1, arcNode *neighb)
     list<Breakpoint>::iterator n1_break = breaks_n1.begin();
     list<Breakpoint>::iterator n2_break = breaks_n2.begin();
 
-    // Premiere facon de faire (a mettre dans une boucle)
-    if (n1_break->c + neighb->arc_c < n2_break->c)
-    {
-        while (min(n1_break->d, neighb->arc_d) >= n2_break->d)
-        {
-            breaks_n2.erase(n2_break);
-            n2_break++;
-        }
-        breaks_n2.insert(n2_break, Breakpoint(min(n1_break->d, neighb->arc_d), n1_break->c + neighb->arc_c));
-    }
-    else if (min(n1_break->d, neighb->arc_d) > n2_break->d)
-    {
-        if (n1_break->c + neighb->arc_c <= n2_break->c)
-        {
-            while (min(n1_break->d, neighb->arc_d) >= n2_break->d)
-            {
-                breaks_n2.erase(n2_break);
-                n2_break++;
-            }
-            breaks_n2.insert(n2_break, Breakpoint(min(n1_break->d, neighb->arc_d), n1_break->c + neighb->arc_c));
-        }
-        else
-        {
-            n1_break++;
-        }
-    }
-    // Peut-etre meilleure facon de faire (en gros) -> A adapter
-    while (n1_break != breaks_n1.end())
-    {
-        while (n2_break->d <= min(n1_break->d, neighb->arc_d))
-        {
-            if (n2_break->c >= n1_break->c + neighb->arc_c)
-                breaks_n2.erase(n2_break);
-            n2_break++;
-        }
-        // Quelle condition ?
-        if (n1_break->c + neighb->arc_c < n2_break->c)
-            breaks_n2.insert(n2_break, Breakpoint(min(n1_break->d, neighb->arc_d), n1_break->c + neighb->arc_c));
-        n1_break++;
-    }
+    double d_ij = neighb->arc_d;
+    double c_ij = neighb->arc_c;
 
-    // 3eme facon de faire
-    while (true)
+    while (n1_break != breaks_n1.end() && n2_break != breaks_n2.end() && n1_break->d < d_ij)
     {
-        // 3.1
-        if (n2_break->d < min(n1_break->d, neighb->arc_d))
+        if (n2_break->d < n1_break->d)
         {
-            if (n2_break->c >= n1_break->c + neighb->arc_c)
+            if (n2_break->c >= n1_break->c + c_ij)
             {
                 breaks_n2.erase(++n2_break);
             }
@@ -66,9 +26,9 @@ void update(Node *n1, arcNode *neighb)
                 n2_break++;
             }
         }
-        else if (n2_break->d == min(n1_break->d, neighb->arc_d))
+        else if (n2_break->d == n1_break->d)
         {
-            if (n2_break->c > n1_break->c + neighb->arc_c)
+            if (n2_break->c > n1_break->c + c_ij)
             {
                 breaks_n2.erase(++n2_break);
             }
@@ -77,59 +37,52 @@ void update(Node *n1, arcNode *neighb)
                 n1_break++;
             }
         }
-        else if (n2_break->c > n1_break->c + neighb->arc_c)
-        {
-            breaks_n2.insert(n2_break, Breakpoint(min(n1_break->d, neighb->arc_d), n1_break->c + neighb->arc_c));
-        }
         else
         {
+            if (n2_break->c > n1_break->c + c_ij)
+                breaks_n2.insert(n2_break, Breakpoint(n1_break->d, n1_break->c + c_ij));
             n1_break++;
-        }
-
-        // 3.2
-        if (n2_break->d < min(n1_break->d, neighb->arc_d))
-        {
-            if (n2_break->c >= n1_break->c + neighb->arc_c)
-            {
-                breaks_n2.erase(++n2_break);
-            }
-            else
-            {
-                n2_break++;
-            }
-        }
-        else if (n2_break->c > n1_break->c + neighb->arc_c)
-        {
-            if (n2_break->d <= min(n1_break->d, neighb->arc_d))
-            {
-                breaks_n2.erase(++n2_break);
-            }
-            else
-            {
-                n2_break++;
-            }
-        }
-        else
-        {
-            n1_break++;
-        }
-
-        // 3.3
-        if (n2_break->d >= min(n1_break->d, neighb->arc_d) && n2_break->c <= n1_break->c + neighb->arc_c)
-        {
-            n1_break++;
-        }
-        else if (
-            (n2_break->d < min(n1_break->d, neighb->arc_d) &&
-             n2_break->c >= n1_break->c + neighb->arc_c) ||
-            (n2_break->c > n1_break->c + neighb->arc_c &&
-             n2_break->d <= min(n1_break->d, neighb->arc_d)))
-        {
-            breaks_n2.erase(++n2_break);
-        }
-        else
-        {
-            breaks_n2.insert(n2_break, Breakpoint(min(n1_break->d, neighb->arc_d), n1_break->c + neighb->arc_c));
         }
     }
+
+    for (; (n1_break != breaks_n1.end() && n1_break->d < d_ij); n1_break++)
+        breaks_n2.push_back(Breakpoint(n1_break->d, n1_break->c + c_ij));
+    if (n1_break != breaks_n1.end())
+    {
+        bool final_break_processed = false;
+        while (n2_break != breaks_n2.end() && !final_break_processed)
+        {
+            if (n2_break->d < d_ij)
+            {
+                if (n2_break->c >= n1_break->c + c_ij)
+                {
+                    breaks_n2.erase(++n2_break);
+                }
+                else
+                {
+                    n2_break++;
+                }
+            }
+            else if (n2_break->d == d_ij)
+            {
+                if (n2_break->c > n1_break->c + c_ij)
+                {
+                    breaks_n2.erase(++n2_break);
+                }
+                else
+                {
+                    final_break_processed = true;
+                }
+            }
+            else
+            {
+                if (n2_break->c > n1_break->c + c_ij)
+                    breaks_n2.insert(n2_break, Breakpoint(d_ij, n1_break->c + c_ij));
+                final_break_processed = true;
+            }
+        }
+        if (n2_break == breaks_n2.end())
+            breaks_n2.push_back(Breakpoint(d_ij, n1_break->c + c_ij));
+    }
+    return;
 }
